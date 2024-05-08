b5a1e6d87c22d6d3796244a058750a14b73327fd
manojkumar6611 <163385086+manojkumar6611@users.noreply.github.com>
2024-05-08 21:25:12 +1000

    Update sighting.py

--- sighting.py
+++ sighting.py
@@ -1,4 +1,4 @@
-## User input
+## Display menu
 def display_menu():
     print("Help")
     print("=" * 4)
@@ -8,3 +8,27 @@ def display_menu():
 
 # Debugging the display_menu() function
 display_menu()
+
+## User input
+
+def display_menu():
+    print("Help")
+    print("=" * 4)
+    print("The following commands are recognized:")
+    print("Display help")
+    print("Exit the application")
+
+def main():
+    while True:
+        command = input("wildlife> ")
+
+        if command == "help":
+            display_menu()
+        elif command == "exit":
+            print("Exiting the application...")
+            break
+        else:
+            print("Error: Invalid command. Please enter 'help' or 'exit'.")
+
+if __name__ == "__main__":
+    main()
