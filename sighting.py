8409264a17350b203ce8edafa796ecb6fb65aa61
manojkumar6611 <163385086+manojkumar6611@users.noreply.github.com>
2024-05-08 21:11:25 +1000

    Update sighting.py

--- sighting.py
+++ sighting.py
@@ -1,1 +1,10 @@
+## User input
+def display_menu():
+    print("Help")
+    print("=" * 4)
+    print("The following commands are recognized:")
+    print("Display help")
+    print("Exit the application")
 
+# Debugging the display_menu() function
+display_menu()
