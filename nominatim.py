5a4e3a242244e121e394c78742864cbad1e15e71
manojkumar6611 <163385086+manojkumar6611@users.noreply.github.com>
2024-05-19 23:17:36 +1000

    Update nominatim.py

--- nominatim.py
+++ nominatim.py
@@ -1,22 +1,10 @@
 import requests
 
-def gps_coordinate(city): 
+def gps_coordinate(city):
     url = f"https://nominatim.openstreetmap.org/search?q={city}&format=json"
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for non-200 status codes
-        
-        data = response.json()
-        
-        if data:
-            return {"latitude": float(data[0]["lat"]), "longitude": float(data[0]["lon"])}
-        else:
-            print("No data found for the city:", city)
-            return None
-    except requests.exceptions.RequestException as err:
-        print("Error fetching data:", err)
-        return None
-    except (IndexError, KeyError) as err:
-        print("Error parsing data:", err)
-        return None
+    response = requests.get(url)
+    data = response.json()
+    if data:
+        return {"latitude": float(data[0]["lat"]), "longitude": float(data[0]["lon"])}
+    return None
 
