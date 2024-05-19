0a775ff8190cff73158c8166a105de4d9270982c
manojkumar6611 <163385086+manojkumar6611@users.noreply.github.com>
2024-05-19 23:29:06 +1000

    Update nominatim.py

--- nominatim.py
+++ nominatim.py
@@ -1,10 +1,2 @@
-import requests
 
-def gps_coordinate(city):
-    url = f"https://nominatim.openstreetmap.org/search?q={city}&format=json"
-    response = requests.get(url)
-    data = response.json()
-    if data:
-        return {"latitude": float(data[0]["lat"]), "longitude": float(data[0]["lon"])}
-    return None
 
